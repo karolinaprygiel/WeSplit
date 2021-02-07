8b54695cd26703691bc2ca2c070a925437dc6eb7
karolinaprygiel <pkarolina890@gmail.com>
2021-02-07 12:52:49 +0100

    initial commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Karolina Prygiel on 07/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Karolina Prygiel on 07/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
